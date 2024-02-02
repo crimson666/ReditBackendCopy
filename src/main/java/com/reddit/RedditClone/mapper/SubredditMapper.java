eea939abe08c6b7f5be3a125488a6080c9ae1bd9
crimson666 <ankurbsws@gmail.com>
2024-02-02 23:39:48 +0530

    Stuff

---
+++ src/main/java/com/reddit/RedditClone/mapper/SubredditMapper.java
@@ -0,0 +1,27 @@
+package com.reddit.RedditClone.mapper;
+
+import com.reddit.RedditClone.dto.SubredditDto;
+import com.reddit.RedditClone.models.Post;
+import com.reddit.RedditClone.models.Subreddit;
+import io.swagger.models.auth.In;
+import org.mapstruct.InheritConfiguration;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface SubredditMapper {
+
+    @Mapping(target = "numberOfPost", expression = "java(mapPost(subreddit.getPosts()))")
+    SubredditDto mapSubredditToDto(Subreddit subreddit);
+
+    default Integer mapPost(List<Post> numberOfPosts){
+        return numberOfPosts.size();
+    };
+
+    @InheritInverseConfiguration
+    @Mapping(target = "posts", ignore = true)
+    Subreddit mapDtoToSubreddit(SubredditDto subredditDto);
+}
