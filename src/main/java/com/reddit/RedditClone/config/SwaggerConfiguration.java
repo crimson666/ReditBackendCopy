c567e7555be9da9887ddc6c1ac52b696d029b864
crimson666 <ankurbsws@gmail.com>
2024-03-12 23:10:32 +0530

    Swagger Doc

---
+++ src/main/java/com/reddit/RedditClone/config/SwaggerConfiguration.java
@@ -0,0 +1,22 @@
+package com.reddit.RedditClone.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    public Docket redditCloneApi(){
+        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).build().apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo(){
+        return new ApiInfoBuilder().title("Reddit clone Apis").version("1.2").description("Api for reddit clone application").contact(new Contact("Ankur Biswas","https://github.com/crimson666?tab=overview&from=2024-03-01&to=2024-03-10","ankurbsws@gmail.com")).license("Apache License Version 2.0").build();
+    }
+}
